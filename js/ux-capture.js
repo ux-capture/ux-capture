4b871f5645e56523995192aeba5ab4cd59c2c74f
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2018-09-05 12:05:50 -0400

    Replaced mocha with Jest, return initializer when included as a module so it can be wrapped around custom window object.

--- js/ux-capture.js
+++ js/ux-capture.js
@@ -1,146 +1,147 @@
 (function(window) {
-  // allow running in Node.js environment
-  if (typeof window === "undefined") {
-    window = {};
-  }
+  const initUXCapture = function(window) {
+    // allow running in Node.js environment
+    if (typeof window === "undefined") {
+      window = {};
+    }
 
-  // prepare base UX Capture object
-  if (typeof window.UX === "undefined") {
-    window.UX = {};
-  }
+    // prepare base UX Capture object
+    if (typeof window.UX === "undefined") {
+      window.UX = {};
+    }
 
-  const isUserTimingSupported =
-    typeof window.performance !== "undefined" &&
-    typeof window.performance.mark !== "undefined" &&
-    typeof window.performance.measure !== "undefined";
+    const isUserTimingSupported =
+      typeof window.performance !== "undefined" &&
+      typeof window.performance.mark !== "undefined" &&
+      typeof window.performance.measure !== "undefined";
 
-  const isConsoleTimeStampSupported =
-    typeof window.console !== "undefined" &&
-    typeof window.console.timeStamp !== "undefined";
+    const isConsoleTimeStampSupported =
+      typeof window.console !== "undefined" &&
+      typeof window.console.timeStamp !== "undefined";
 
-  let expectedZones = [];
+    let expectedZones = [];
 
-  let getMark = markLabel =>
-    expectedZones.reduce((expectedMark, expectedZone) => {
-      // already found in previous zone, no need to be iterating over current zone's marks
-      return expectedMark
-        ? expectedMark
-        : expectedZone.marks.find(
-            expectedMark => expectedMark.label === markLabel
-          );
-    }, null);
-
-  let onMark;
-  let onMeasure;
-
-  window.UX.expect = function(zones) {
-    expectedZones = zones.map(zone => {
-      // only create promises if zone contains any marks, otherwise just ignore it
-      if (zone.marks && zone.marks.length > 0) {
-        // wrap each string in the array into an object so we can attach record() methods to them
-        zone.marks = zone.marks.map(label => ({ label }));
-
-        // create a promise for each expected mark
-        const promises = zone.marks.map(
-          mark =>
-            new Promise((resolve, reject) => {
-              mark.record = () => {
-                if (isUserTimingSupported) {
-                  // record the mark using W3C User Timing API
-                  window.performance.mark(mark.label);
-
-                  // if callback is specified, call it with mark label
-                  if (onMark) {
-                    onMark(mark.label);
-                  }
-                }
-
-                /**
-                 * Report same mark on Chrome/Firefox timeline
-                 *
-                 * keep in mind, these timestams are counted from timeline recording start
-                 * while UserTiming marks are counted from navigationStart event
-                 * however visually, they all will be offset by the same amount of time and align vertically on the charts
-                 *
-                 * (we'd provide a helper to highlight discrepancy, but unfortunately,
-                 * there is no way to know when in timeline did navigationStart event occured)
-                 */
-                if (isConsoleTimeStampSupported) {
-                  console.timeStamp(mark.label);
-                }
-
-                // remember last mark that was recorded within a zone, overriding previous one
-                zone.lastMarkLabel = mark.label;
-
-                resolve(mark.label);
-              };
-            })
-        );
-
-        // only if all marks were recorded (and promises resolved), go ahead and record the measure ending with last recorded mark
-        Promise.all(promises).then(() => {
-          if (isUserTimingSupported) {
-            // record a measure using W3C User Timing API
-            window.performance.measure(
-              zone.label,
-              "navigationStart",
-              zone.lastMarkLabel
+    let getMark = markLabel =>
+      expectedZones.reduce((expectedMark, expectedZone) => {
+        // already found in previous zone, no need to be iterating over current zone's marks
+        return expectedMark
+          ? expectedMark
+          : expectedZone.marks.find(
+              expectedMark => expectedMark.label === markLabel
             );
+      }, null);
 
-            // if callback is specified, call it with zone label
-            if (onMeasure) {
-              onMeasure(zone.label);
+    let onMark;
+    let onMeasure;
+
+    /**
+     * Sets expected marks and corresponding zones for current view
+     *
+     * @TODO re-evaluate if we should allow multiple executions of this method
+     */
+    window.UX.expect = function(zones) {
+      expectedZones = zones.map(zone => {
+        // only create promises if zone contains any marks, otherwise just ignore it
+        if (zone.marks && zone.marks.length > 0) {
+          // wrap each string in the array into an object so we can attach record() methods to them
+          zone.marks = zone.marks.map(label => ({ label }));
+
+          // create a promise for each expected mark
+          const promises = zone.marks.map(
+            mark =>
+              new Promise((resolve, reject) => {
+                mark.record = () => {
+                  if (isUserTimingSupported) {
+                    // record the mark using W3C User Timing API
+                    window.performance.mark(mark.label);
+
+                    // if callback is specified, call it with mark label
+                    if (onMark) {
+                      onMark(mark.label);
+                    }
+                  }
+
+                  /**
+                   * Report same mark on Chrome/Firefox timeline
+                   *
+                   * keep in mind, these timestamps are counted from timeline recording start
+                   * while UserTiming marks are counted from navigationStart event
+                   * however visually, they all will be offset by the same amount of time and align vertically on the charts
+                   *
+                   * (we'd provide a helper to highlight discrepancy, but unfortunately,
+                   * there is no way to know when in timeline did navigationStart event occured)
+                   */
+                  if (isConsoleTimeStampSupported) {
+                    console.timeStamp(mark.label);
+                  }
+
+                  // remember last mark that was recorded within a zone, overriding previous one
+                  zone.lastMarkLabel = mark.label;
+
+                  resolve(mark.label);
+                };
+              })
+          );
+
+          // only if all marks were recorded (and promises resolved), go ahead and record the measure ending with last recorded mark
+          Promise.all(promises).then(() => {
+            if (isUserTimingSupported) {
+              // record a measure using W3C User Timing API
+              // null start mark defaults to navigationStart - makes it work in environments where names mark is not set
+              window.performance.measure(zone.label, null, zone.lastMarkLabel);
+
+              // if callback is specified, call it with zone label
+              if (onMeasure) {
+                onMeasure(zone.label);
+              }
             }
-          }
-        });
+          });
 
-        return zone;
+          return zone;
+        }
+      });
+    };
+
+    window.UX.mark = function(markLabel) {
+      const mark = getMark(markLabel);
+
+      if (mark) {
+        mark.record();
       }
-    });
+    };
+
+    window.UX.config = function(configuration) {
+      if (!configuration || typeof configuration !== "object") {
+        return;
+      }
+
+      if (typeof configuration.onMark === "function") {
+        onMark = configuration.onMark;
+      }
+
+      if (typeof configuration.onMeasure === "function") {
+        onMeasure = configuration.onMeasure;
+      }
+    };
+
+    return window.UX;
   };
 
-  window.UX.mark = function(markLabel) {
-    const mark = getMark(markLabel);
-
-    if (mark) {
-      mark.record();
-    }
-  };
-
-  window.UX.config = function(configuration) {
-    if (!configuration || typeof configuration !== "object") {
-      return;
-    }
-
-    if (typeof configuration.onMark === "function") {
-      onMark = configuration.onMark;
-    }
-
-    if (typeof configuration.onMeasure === "function") {
-      onMeasure = configuration.onMeasure;
-    }
-  };
-
-  //
-  // Export UX Capture to the appropriate location.
-  //
-  // When included directly via a script tag in the browser, we're good as we've been
-  // updating the window.UX object.
-  //
+  // Export initializer function for UX Capture to the appropriate location.
   if (typeof define === "function" && define.amd) {
-    //
     // AMD / RequireJS
-    //
     define([], function() {
-      return window.UX;
+      return initUXCapture;
     });
   } else if (
     typeof module !== "undefined" &&
     typeof module.exports !== "undefined"
   ) {
-    //
     // Node.js
-    //
-    module.exports = window.UX;
+    module.exports = initUXCapture;
+  } else {
+    // When included directly via a script tag in the browser,
+    // run initialization function with window object.
+    initUXCapture(window);
   }
 })(typeof window !== "undefined" ? window : undefined);
--- test/ux-capture.test.js
+++ test/ux-capture.test.js
@@ -1,25 +1,11 @@
-(function(window) {
-  //
-  // Run in either Mocha, Karma or Browser environments
-  //
-  if (typeof window === "undefined") {
-    window = {};
-  }
+// UserTiming polyfill to override broken jsdom performance API
+window.performance = require("usertiming");
 
-  // UserTiming polyfill
-  window.performance = window.performance
-    ? window.performance
-    : require("usertiming");
+// set up global UX object
+const UX = require("../js/ux-capture")(window);
 
-  // set up global UX object
-  const UX = window.UX ? window.UX : require("../js/ux-capture.min.js");
-
-  // assertion library
-  const expect = global.expect ? global.expect : require("expect");
-
-  describe("UX Capture", function() {
-    it("should be available as UX global variable", function() {
-      expect(UX);
-    });
+describe("UX Capture", function() {
+  it("should be available as UX global variable", () => {
+    expect(UX);
   });
-})(typeof window !== "undefined" ? window : undefined);
+});
