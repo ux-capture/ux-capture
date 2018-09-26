7a40da790dae16d6e6bef2528afb779ae2f8012d
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2018-09-26 10:58:07 -0400

    Added ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./js/src/ux-capture.js",
+  mode: "production",
   output: {
     filename: "ux-capture.min.js",
     path: path.resolve(__dirname, "js")
@@ -10,7 +11,14 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: "pre",
         test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: "babel-loader"
       }
     ]
