f6e9500f4cef3843ed7ef1308fd76f91a48fc9fa
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2018-10-05 18:05:40 -0400

    Updated comment to remove reference to Promises

--- js/src/View.js
+++ js/src/View.js
@@ -3,7 +3,7 @@ import Zone from "./Zone";
 export default class View {
   constructor({ onMark, onMeasure, zoneConfigs }) {
     this.expectedZones = zoneConfigs.map(zoneConfig => {
-      // only create promises if zone contains any marks, otherwise just ignore it
+      // only create zones if configuration contains marks, otherwise just ignore it
       if (zoneConfig.marks && zoneConfig.marks.length > 0) {
         return new Zone({
           onMark,
