f7e5c36cbc5d7e76b16ebaef1f33bdbdfc9f13f7
Sadaf Shahsahebi <sadaf@meetup.com>
2018-10-15 09:24:51 -0700

    jsdoc style comments for View.js

--- js/src/View.js
+++ js/src/View.js
@@ -1,12 +1,16 @@
 import Zone from "./Zone";
 
 /**
- * View is a collection of zones representing one page view or interactive view
+ * A `View` is a collection of zones representing a single
+ * page view or interactive view.
+ *
+ * Refer to ux-capture README glossary for view definitions.
+ * @see: https://github.com/meetup/ux-capture#glossary
  */
 export default class View {
   constructor({ onMark, onMeasure, zoneConfigs }) {
     this.expectedZones = zoneConfigs.map(zoneConfig => {
-      // only create zones if configuration contains marks, otherwise just ignore it
+      // Create a new zone only if configuration contains marks
       if (zoneConfig.marks && zoneConfig.marks.length > 0) {
         return new Zone({
           onMark,
