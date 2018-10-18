b7a39af48da08636abccbdb7fb0ff317296b6ab7
Sadaf Shahsahebi <sadaf@meetup.com>
2018-10-18 13:04:08 -0700

    PR: comments for UXBase class

--- js/src/UXBase.js
+++ js/src/UXBase.js
@@ -1,4 +1,14 @@
-
+/**
+ * `UXBase` is a parent class that constructs a `this.props`
+ * interface for all classes that extend it.
+ *
+ * This pattern permits subclasses to avoid needing
+ * constructors only for property assignment.
+ *
+ * By itself, `UXBase` has no utility.
+ *
+ * @see: Zone.js, View.js
+ */
 export default class UXBase {
   constructor(props) {
     this.props = props;
