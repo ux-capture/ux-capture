33cbe8819270ac7c48e13df3a7612b75a209a0c5
Sadaf Shahsahebi <sadaf@meetup.com>
2018-10-25 07:56:21 -0700

    remove AMD / RequireJS module loading

--- js/src/ux-capture.js
+++ js/src/ux-capture.js
@@ -1,18 +1,7 @@
-/* eslint-disable no-undef */
-
 import UXCapture from './UXCapture';
 
 (function() {
-	// Export initializer function for UX Capture to the appropriate location.
-	if (typeof define === 'function' && define.amd) {
-		// AMD / RequireJS
-		define([], function() {
-			return UXCapture;
-		});
-	} else if (
-		typeof module !== 'undefined' &&
-		typeof module.exports !== 'undefined'
-	) {
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 		// Node.js
 		module.exports = UXCapture;
 	} else {
