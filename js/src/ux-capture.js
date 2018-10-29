49f67a5be2902060acd2e1ca3b43f271b46156c3
Sadaf Shahsahebi <sadaf@meetup.com>
2018-10-29 07:29:19 -0700

    Revert "remove AMD / RequireJS module loading"
    This reverts commit 33cbe8819270ac7c48e13df3a7612b75a209a0c5.

--- js/src/ux-capture.js
+++ js/src/ux-capture.js
@@ -1,7 +1,18 @@
+/* eslint-disable no-undef */
+
 import UXCapture from './UXCapture';
 
 (function() {
-	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	// Export initializer function for UX Capture to the appropriate location.
+	if (typeof define === 'function' && define.amd) {
+		// AMD / RequireJS
+		define([], function() {
+			return UXCapture;
+		});
+	} else if (
+		typeof module !== 'undefined' &&
+		typeof module.exports !== 'undefined'
+	) {
 		// Node.js
 		module.exports = UXCapture;
 	} else {
