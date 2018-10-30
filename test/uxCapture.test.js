b24f26eafe8169e79b665e03beaa5038186ec24d
Sadaf Shahsahebi <sadaf@meetup.com>
2018-10-30 12:46:29 -0700

    fix mark tests. move to uxCapture.test.js

--- test/uxCapture.test.js
+++ test/uxCapture.test.js
@@ -1,4 +1,5 @@
 import UXCapture from '../js/src/UXCapture';
+import ExpectedMark from '../js/src/ExpectedMark';
 
 // UserTiming polyfill to override broken jsdom performance API
 window.performance = require('usertiming');
@@ -13,18 +14,18 @@ window.performance.timing = {
 window.requestAnimationFrame = jest.fn(cb => cb());
 window.setTimeout = jest.fn(cb => cb());
 
+// using console.timeStamp for testing only
+console.timeStamp = jest.fn();
+
 const MOCK_MEASURE_1 = 'ux-mock-measure_1';
 const MOCK_MARK_1_1 = 'ux-mock-mark_1_1';
 const MOCK_MARK_1_2 = 'ux-mock-mark_1_2';
 
 const MOCK_MEASURE_2 = 'ux-mock-measure_2';
-const MOCK_MARK_2_1 = 'ux-mock-mark_2_1';
-
 const MOCK_MEASURE_3 = 'ux-mock-measure_3';
-const MOCK_MARK_3_1 = 'ux-mock-mark_3_1';
 
-const MOCK_MEASURE_4 = 'ux-mock-measure_3';
-const MOCK_MARK_4_1 = 'ux-mock-mark_3_1';
+const MOCK_UNEXPECTED_MARK = 'ux-unexpected-mark';
+const MOCK_MARK_MULTIPLE = 'ux-mock-mark_multiple';
 
 const onMark = jest.fn();
 const onMeasure = jest.fn();
@@ -38,7 +39,8 @@ describe('UXCapture', () => {
 			});
 		});
 
-		afterAll(() => {
+		beforeEach(() => {
+			ExpectedMark.clearExpectedMarks();
 			onMark.mockClear();
 			onMeasure.mockClear();
 		});
@@ -85,6 +87,12 @@ describe('UXCapture', () => {
 	});
 
 	describe('create', () => {
+		beforeEach(() => {
+			ExpectedMark.clearExpectedMarks();
+			onMark.mockClear();
+			onMeasure.mockClear();
+		});
+
 		it('Should throw an error if non-object is passed', () => {
 			expect(() => {
 				UXCapture.create();
@@ -109,11 +117,11 @@ describe('UXCapture', () => {
 				UXCapture.create({ onMeasure: 'not a function' });
 				UXCapture.startView([
 					{
-						name: MOCK_MEASURE_2,
-						marks: [MOCK_MARK_2_1],
+						name: MOCK_MEASURE_1,
+						marks: [MOCK_MARK_1_1],
 					},
 				]);
-				UXCapture.mark(MOCK_MARK_2_1);
+				UXCapture.mark(MOCK_MARK_1_1);
 			}).toThrow();
 		});
 
@@ -121,26 +129,129 @@ describe('UXCapture', () => {
 			UXCapture.create({ onMark });
 			UXCapture.startView([
 				{
-					name: MOCK_MEASURE_3,
-					marks: [MOCK_MARK_3_1],
+					name: MOCK_MEASURE_1,
+					marks: [MOCK_MARK_1_1],
 				},
 			]);
-			UXCapture.mark(MOCK_MARK_3_1);
+			UXCapture.mark(MOCK_MARK_1_1);
 
-			expect(onMark).toHaveBeenCalledWith(MOCK_MARK_3_1);
+			expect(onMark).toHaveBeenCalledWith(MOCK_MARK_1_1);
 		});
 
 		it('Should be able to configure onMeasure handler', () => {
 			UXCapture.create({ onMeasure });
 			UXCapture.startView([
 				{
-					name: MOCK_MEASURE_4,
-					marks: [MOCK_MARK_4_1],
+					name: MOCK_MEASURE_1,
+					marks: [MOCK_MARK_1_1],
 				},
 			]);
-			UXCapture.mark(MOCK_MARK_4_1);
+			UXCapture.mark(MOCK_MARK_1_1);
 
-			expect(onMeasure).toHaveBeenCalledWith(MOCK_MEASURE_4);
+			expect(onMeasure).toHaveBeenCalledWith(MOCK_MEASURE_1);
+		});
+	});
+
+	describe('mark', () => {
+		beforeAll(() => {
+			UXCapture.create({ onMark, onMeasure });
+		});
+
+		beforeEach(() => {
+			ExpectedMark.clearExpectedMarks();
+			onMark.mockClear();
+			onMeasure.mockClear();
+
+			UXCapture.startView([
+				{
+					name: MOCK_MEASURE_1,
+					marks: [MOCK_MARK_1_1, MOCK_MARK_1_2],
+				},
+				{
+					name: MOCK_MEASURE_2,
+					marks: [MOCK_MARK_MULTIPLE],
+				},
+				{
+					name: MOCK_MEASURE_3,
+					marks: [MOCK_MARK_MULTIPLE],
+				},
+			]);
+		});
+
+		it('must mark user timing api timeline', () => {
+			UXCapture.mark(MOCK_MARK_1_1);
+
+			expect(
+				window.performance
+					.getEntriesByType('mark')
+					.find(mark => mark.name === MOCK_MARK_1_1)
+			).toBeTruthy();
+		});
+
+		it('should trigger recording of a measure if last mark in chain', () => {
+			UXCapture.mark(MOCK_MARK_1_1);
+			UXCapture.mark(MOCK_MARK_1_2);
+
+			expect(
+				window.performance
+					.getEntriesByType('measure')
+					.find(measure => measure.name === MOCK_MEASURE_1)
+			).toBeTruthy();
+		});
+
+		it("should fire a console.timeStamp if it's available", () => {
+			console.timeStamp.mockClear();
+
+			UXCapture.mark(MOCK_MARK_1_1);
+
+			expect(console.timeStamp).toHaveBeenCalledWith(MOCK_MARK_1_1);
+		});
+
+		it('should call a custom mark callback when provided', () => {
+			UXCapture.mark(MOCK_MARK_1_1);
+
+			expect(onMark).toHaveBeenCalledWith(MOCK_MARK_1_1);
+		});
+
+		it('should not record a mark that is not expected', () => {
+			UXCapture.mark(MOCK_UNEXPECTED_MARK);
+			expect(
+				window.performance
+					.getEntriesByType('mark')
+					.find(mark => mark.name === MOCK_UNEXPECTED_MARK)
+			).not.toBeTruthy();
+		});
+
+		it('should contribute to multiple measures if same mark is defined for multiple zones', () => {
+			UXCapture.mark(MOCK_MARK_MULTIPLE);
+
+			expect(
+				window.performance
+					.getEntriesByType('measure')
+					.find(measure => measure.name === MOCK_MEASURE_2)
+			).toBeTruthy();
+
+			expect(
+				window.performance
+					.getEntriesByType('measure')
+					.find(measure => measure.name === MOCK_MEASURE_3)
+			).toBeTruthy();
+		});
+
+		it('must work when called without waiting for next paint flag', () => {
+			UXCapture.mark(MOCK_MARK_1_1, false);
+
+			expect(
+				window.performance
+					.getEntriesByType('mark')
+					.find(mark => mark.name === MOCK_MARK_1_1)
+			).toBeTruthy();
+
+			expect(
+				window.performance
+					.getEntriesByType('measure')
+					.find(measure => measure.name === MOCK_MEASURE_1)
+			).toBeTruthy();
 		});
 	});
 });
