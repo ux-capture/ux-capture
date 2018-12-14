48d43583a7bec42ef2e1f1ca89d4dc3d23d84135
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2018-12-14 09:48:49 -0500

    Added source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
-const webpack = require("webpack"); // to access built-in plugins
-const path = require("path");
+const webpack = require('webpack'); // to access built-in plugins
+const path = require('path');
 
 module.exports = {
-  entry: "./src/ux-capture.js",
-  mode: "production",
-  output: {
-    filename: "ux-capture.min.js",
-    path: path.resolve(__dirname, "lib")
-  },
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      }
-    ]
-  },
-  plugins: [new webpack.ProgressPlugin()]
+	entry: './src/ux-capture.js',
+	devtool: 'source-map',
+	mode: 'production',
+	output: {
+		filename: 'ux-capture.min.js',
+		path: path.resolve(__dirname, 'lib'),
+	},
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+		],
+	},
+	plugins: [new webpack.ProgressPlugin()],
 };
