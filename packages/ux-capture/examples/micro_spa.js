67ae038ee91e6289ad498c55b2123e17dc00b4c1
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2019-01-16 11:35:45 -0500

    Made sample more progressive to visually match zone progression. Added heavier image to load visibly slow on throttled network.

--- packages/ux-capture/examples/micro_spa.js
+++ packages/ux-capture/examples/micro_spa.js
@@ -1,51 +1,61 @@
 // emulating a long-running JS event attachment, e.g. React re-hydration
 setTimeout(() => {
-	document.getElementById('morelink').addEventListener('click', function(e) {
+	const morelink = document.getElementById('morelink');
+	morelink.style.display = 'inline';
+	morelink.addEventListener('click', function(e) {
 		e.preventDefault();
 
 		alert('More kittens there');
 	});
 
-	window.UXCapture.mark('ux-handler-moreclickable');
-}, 300);
+	UXCapture.mark('ux-handler-moreclickable');
+}, 500);
 
 // mini-SPA
-document.querySelector('a[href="page2.html"]').addEventListener('click', e => {
-	e.preventDefault();
+setTimeout(() => {
+	const page2link = document.querySelector('a[href="page2.html"]');
+	page2link.style.display = 'inline';
+	page2link.addEventListener('click', e => {
+		e.preventDefault();
 
-	// triggers initial mark
-	UXCapture.startTransition();
+		// triggers initial mark
+		UXCapture.startTransition();
 
-	document.getElementById('content').innerHTML = '<i>... loading page 2 ...</i>';
-
-	setTimeout(() => {
-		[
-			'ux-text-intro',
-			'ux-text-story',
-			'ux-image-inline-kitten',
-			'ux-image-onload-kitten',
-			'ux-text-story-details-link',
-			'ux-handler-moreclickable',
-			'ux-text-page2',
-		].map(UXCapture.clearMarks);
-
-		// sets new view expectations
-		UXCapture.startView([
-			{
-				name: 'ux-destination-verified',
-				marks: ['ux-text-title'],
-			},
-			{
-				name: 'ux-primary-content-displayed',
-				marks: ['ux-text-page2-content'],
-			},
-		]);
+		document.getElementById('content').innerHTML =
+			'<i>... loading page 2 ...</i>';
 
 		setTimeout(() => {
-			document.getElementById('content').innerHTML =
-				'<p id="page2content">SPA transition to page 2 successful</p>';
+			[
+				'ux-text-intro',
+				'ux-text-story',
+				'ux-image-inline-kitten',
+				'ux-image-onload-kitten',
+				'ux-text-story-details-link',
+				'ux-handler-moreclickable',
+				'ux-text-page2',
+				'ux-handler-page2-clickable',
+			].map(UXCapture.clearMarks);
 
-			UXCapture.mark('ux-text-page2-content');
-		}, 900); // 1s delay between startTransition() and Primary Content Available
-	}, 100); // 100ms delay between startTransition() and startView();
-});
+			// sets new view expectations
+			UXCapture.startView([
+				{
+					name: 'ux-destination-verified',
+					marks: ['ux-text-title'],
+				},
+				{
+					name: 'ux-primary-content-displayed',
+					marks: ['ux-text-page2-content'],
+				},
+			]);
+
+			setTimeout(() => {
+				document.getElementById('content').innerHTML =
+					'<p id="page2content">SPA transition to page 2 successful</p>';
+
+				UXCapture.mark('ux-text-page2-content');
+			}, 900); // 1s delay between startTransition() and Primary Content Available
+		}, 100); // 100ms delay between startTransition() and startView();
+	});
+
+	UXCapture.mark('ux-handler-page2-clickable');
+}, 1000);
