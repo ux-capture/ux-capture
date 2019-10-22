266244b3b82b5a2b8b78cbf1053cb8ad348a3f08
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2019-10-22 10:15:50 -0400

    Added key to ux capture script

--- packages/gatsby-ux-capture-example/gatsby-ssr.js
+++ packages/gatsby-ux-capture-example/gatsby-ssr.js
@@ -1,11 +1,15 @@
+import React from "react";
+
 import fs from "fs";
 
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
 	const headComponents = getHeadComponents();
 
 	const uxCaptureFilename = require.resolve("@meetup/ux-capture/lib/ux-capture.min.js");
 	const uxCaptureJS = fs.readFileSync(uxCaptureFilename, "utf8");
 
-	headComponents.push(<script dangerouslySetInnerHTML={{ __html: uxCaptureJS }} />);
+	headComponents.push(
+		<script key="ux-capture" dangerouslySetInnerHTML={{ __html: uxCaptureJS }} />
+	);
 	replaceHeadComponents(headComponents);
 };
