ed5e020572fce5ecb989bb750a8429b62e6fc930
Najati <najati.imam@wework.com>
2019-12-04 17:00:29 -0500

    Add export declarations to types, bump package

--- packages/react-ux-capture/types/index.d.ts
+++ packages/react-ux-capture/types/index.d.ts
@@ -1,33 +1,33 @@
-declare interface UXCaptureZones extends UXCaptureGenericZones{
+export declare interface UXCaptureZones extends UXCaptureGenericZones{
 	destinationVerified: UXCaptureZone,
 	primaryContentDisplayed: UXCaptureZone,
 	primaryActionAvailable: UXCaptureZone,
 	secondaryContentDisplayed: UXCaptureZone
 }
 
-declare type UXCaptureZone = string[];
+export declare type UXCaptureZone = string[];
 
-declare interface UXCaptureGenericZones {
+export declare interface UXCaptureGenericZones {
 	[x: string]: UXCaptureZone
 }
 
-declare type UXCapture = (UXCaptureGenericZones) => React.ReactElement
-declare type callbackType = (label: string) => void;
+export declare type UXCapture = (UXCaptureGenericZones) => React.ReactElement
+export declare type callbackType = (label: string) => void;
 
-declare type UXCaptureCreateProps = {
+export declare type UXCaptureCreateProps = {
 	onMark?: callbackType,
 	onMeasure?: callbackType,
 	};
 
-declare type UXCaptureCreate = (UXCaptureCreateProps) => React.ReactElement
+export declare type UXCaptureCreate = (UXCaptureCreateProps) => React.ReactElement
 
-declare type UXCaptureFontProps = {
+export declare type UXCaptureFontProps = {
 	fontFamily: string,
 	mark: string,
 };
 
-declare type UXCaptureFont = (UXCaptureFontProps) => React.ReactElement
+export declare type UXCaptureFont = (UXCaptureFontProps) => React.ReactElement
 
-declare type UXCaptureImageLoadProps = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & { mark: string };
+export declare type UXCaptureImageLoadProps = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & { mark: string };
 
-declare type UXCaptureImageLoad = (UXCaptureImageLoadProps) => React.ReactElement
+export declare type UXCaptureImageLoad = (UXCaptureImageLoadProps) => React.ReactElement
