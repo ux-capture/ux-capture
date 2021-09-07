c19edc68336fe76b9d3a9de8e1b613efb3acd20a
Sergey Chernyshev <schernyshev@etsy.com>
2021-09-07 18:10:17 -0400

    Using spread instead of legacy apply trick.

--- packages/ux-capture/src/View.js
+++ packages/ux-capture/src/View.js
@@ -16,10 +16,10 @@ function View(props) {
 // to exisiting zones or new zones or both
 View.prototype.update = function (zoneConfigs) {
 	// Append configuration, same way we append the zones themselves
-	this.props.zoneConfigs.push.apply(this.props.zoneConfigs, zoneConfigs);
+	this.props.zoneConfigs.push(...zoneConfigs);
 
 	// Append new zones to existing config
-	this.expectedZones.push.apply(this.expectedZones, this.setZones(zoneConfigs));
+	this.expectedZones.push(...this.setZones(zoneConfigs));
 };
 
 View.prototype.setZones = function (zoneConfigs) {
